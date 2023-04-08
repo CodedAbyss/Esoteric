bffdac4eefd6f975e95a5aef0d7630bc1864f30e
CodedAbyss <justindewitt_@outlook.com>
2023-04-08 13:03:53 -0400

    added bf interpreter

---
+++ brainfuck.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#define b {c=getc(f);l+=(c==91)-(c==93);}
+#define c(a)case a;break;
+int main(int n,char**v){char m[256]={},*d=m;FILE*f=fopen(v[1],"rb");for(int l,c=getc(f);c>-1;){switch(c){c(62:d++)c(60:d--)c(43:(*d)++)
+c(45:(*d)--)c(46:putchar(*d))c(44:*d=getchar())c(91:if(!*d)for(l=1;l;)b)c(93:for(l=-1;l&&!fseek(f,-2,1);)b continue)}c=getc(f);}}
